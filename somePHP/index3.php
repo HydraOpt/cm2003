5b47c93f84ab75c7f38053ffbd4788964095c947
HydraOpt <a.gadaleta@rgu.ac.uk>
2017-03-16 20:16:49 +0000

    a

--- somePHP/index3.php
+++ somePHP/index3.php
@@ -3,12 +3,10 @@
 
 <head>
     <title>My First Webpage</title>
-    !
     <meta charset="utf-8" />
     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" /> !
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 </head>
-!
 
 <body>
     <div>
@@ -23,7 +21,7 @@
             echo "Mail sent successfully!";
           } else {
               echo "Mail not sent!";
-            }
+        }
             ?>
     </div>
 </body>

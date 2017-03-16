443085852c42acedce558d240781278669d37257
HydraOpt <a.gadaleta@rgu.ac.uk>
2017-03-16 18:52:37 +0000

    a

--- somePHP/index.php
+++ somePHP/index.php
@@ -1,1 +1,23 @@
-Hello world!
+<?php
+  $test = "a variable and here is an escaped \"";
+  $test2 = " other text";
+  $test3 = 44;
+  $test4 = "Phil";
+  $test5 = "test4"
+  echo $test.$test2;
+  echo "<br /><br /> ";
+  echo $test3/22;
+  echo "<br /><br /> ";
+  echo "His name is $test4 or ".$test4;
+  echo "<br /><br /> ";
+  echo $$test5;
+  echo "<br /><br /> ";
+
+  $myArray=array("pizza", "chocolate");
+  $myArray[]="mandolino";
+  print_r($myArray);
+  echo "<br /><br /> ";
+  $array2 = array("Framce" => "Fremch, "USA" => "Americans");
+  unset($array["USA"]);
+  print_r($array2);
+?>

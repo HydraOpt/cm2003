8b2c9810b3e608a075b215483653c23af155a599
HydraOpt <a.gadaleta@rgu.ac.uk>
2017-03-16 19:51:38 +0000

    a

--- somePHP/index2.php
+++ somePHP/index2.php
@@ -1,4 +1,21 @@
 <?php
- echo "does it work?;
+ $number = 1;
+ $number2 = 2;
+
+ if($number == 1 OR $number2 ==2){
+   echo "lala";
+ } else {
+   echo "lolo";
+ }
+
+ $i=0;
+ $array=array("apple", "banana");
+
+/*while($i<20)*/
+/*this version will end at the end of the array*/
+ while($array[$i]){
+   echo "Key: $i Value: $array[$i]";
+   $i++;
+ }
 
  ?>

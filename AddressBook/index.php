4dde5e235c1fc6b3f45efc284767fb92c91f04c7
HydraOpt <a.gadaleta@rgu.ac.uk>
2017-03-17 17:45:14 +0000

    a

--- AddressBook/index.php
+++ AddressBook/index.php
@@ -2,54 +2,44 @@
 <html>
 
 <head>
-    <title>Address Book</title>
+    <title>My First Webpage</title>
     <meta charset="utf-8"/>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+    <style>
+        .form {
+            border: 1px solid black;
+            margin-left: 10px;
+            margin-top: 10px;
+        }
+        </style>
 </head>
 
 <body>
-<div>
+
+
+
     <?php
-    // Now the element are submited in POST (array)
-    $names =array("Fred", "Rob", "Ian");
-
-    // If "submit" element of POST (array) is not empty
-    if($_POST["submit"]) {
-        // If "name" element of POST (array) is not empty (true)
-        if ($_POST["name"]) {
-            //foreach
-            foreach($names as $name /* => $value is not inserted couse we are not interested*/){
-                // if the var "name" in POST is equal to the element with placeholder $name in array $names
-                if($_POST["name"]==$name){
-                    echo "I know you, your name is ".$name;
-                    //declaring a boolean var to true
-                    $knowyou =1;
-                }
-
-            }
-            if(!$knowyou){
-                echo "I don't know you";
-            }
-         // If "name" is empty
-        } else{
-            echo "No name";
-        }
-
+    /*
+    echo "does it work at all";
+    $emailTo = "alessio.gadaleta@gmail.com";
+    $subject = "You are good!";
+    $body = "You really are. L ammiratore segreto";
+    $headers = "From: ammiratoremisterioso@callme.com";
+    if (mail($emailTo, $subject, $body, $headers)) {
+        echo "Mail sent successfully!";
+    } else {
+        echo "Mail not sent!";
     }
-
+    */
     ?>
-<!-- In this case the form will submit using the POST method instead of GET(default) -->
-    <form method ="post">
-        <label for="name">Name</label>
-<!--            create a variable named name and with submit it trigger a GET -->
-        <input name="name" type="text" value="test"/>
-        <input type="submit" name="submit" value="Submit your name"/>
 
-    </form>
+    <div class="form">
+        </div>
 
-</div>
 </body>
+
 </html>
 
 
---
+++ somePHP/simplePOST.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>Address Book</title>
+    <meta charset="utf-8"/>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+</head>
+
+<body>
+<div>
+    <?php
+    // Now the element are submited in POST (array)
+    $names =array("Fred", "Rob", "Ian");
+
+    // If "submit" element of POST (array) is not empty
+    if($_POST["submit"]) {
+        // If "name" element of POST (array) is not empty (true)
+        if ($_POST["name"]) {
+            //foreach
+            foreach($names as $name /* => $value is not inserted couse we are not interested*/){
+                // if the var "name" in POST is equal to the element with placeholder $name in array $names
+                if($_POST["name"]==$name){
+                    echo "I know you, your name is ".$name;
+                    //declaring a boolean var to true
+                    $knowyou =1;
+                }
+
+            }
+            if(!$knowyou){
+                echo "I don't know you";
+            }
+         // If "name" is empty
+        } else{
+            echo "No name";
+        }
+
+    }
+
+    ?>
+<!-- In this case the form will submit using the POST method instead of GET(default) -->
+    <form method ="post">
+        <label for="name">Name</label>
+<!--            create a variable named name and with submit it trigger a GET -->
+        <input name="name" type="text"/>
+        <input type="submit" name="submit" value="Submit your name"/>
+
+    </form>
+
+</div>
+</body>
+</html>
+
+

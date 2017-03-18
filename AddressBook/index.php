a1c8a34909c3949669fe71b143073fca9861a4e9
HydraOpt <a.gadaleta@rgu.ac.uk>
2017-03-18 16:16:28 +0000

    a

--- AddressBook/index.php
+++ AddressBook/index.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
     if($_POST["submit"]){
-        $result = 'Form Submitted';
+        $result = '<div>Form Submitted</div>';
     }
     else {
         $result = 'Something Wrong';
